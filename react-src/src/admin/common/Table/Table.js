caae18158facf3b2f752dd7df8522de3cc2eab94
Muzammil Hussain <smhz101@gmail.com>
2023-09-18 23:52:18 +0500

    Added JSdoc and internationalization to the Table component.

--- react-src/src/admin/common/Table/Table.js
+++ react-src/src/admin/common/Table/Table.js
@@ -1,14 +1,23 @@
-// Table.js
 import React from "react";
+import { __ } from "@wordpress/i18n";
 import "./Table.css";
 
+/**
+ * Table component to render a table with headers and data.
+ *
+ * @param {Object} props Component properties.
+ * @param {Array<{label: string, key: string}>} props.headers Table column headers.
+ * @param {Array<Object>} props.data Table data rows.
+ * @param {function(Object, string): string} [props.renderCell] Optional function to render a table cell.
+ * @returns {JSX.Element} Rendered table.
+ */
 function Table({ headers, data, renderCell }) {
   return (
     <table className="urm-table">
       <thead>
         <tr>
           {headers.map((header, index) => (
-            <th key={index}>{header.label}</th>
+            <th key={index}>{__(header.label, "user-request-manager")}</th>
           ))}
         </tr>
       </thead>

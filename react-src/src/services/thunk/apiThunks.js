65fc67929e1dcc5c140cdc7c2cfb8ba631fd4045
Muzammil Hussain <smhz101@gmail.com>
2023-09-19 01:28:52 +0500

    Added internationalization and JSDocs to thunk actions for settings.

--- react-src/src/services/thunk/apiThunks.js
+++ react-src/src/services/thunk/apiThunks.js
@@ -1,10 +1,16 @@
 import { fetchSettings, saveSettings } from "../api";
+import { __ } from "@wordpress/i18n";
 
 import { setGeneralSettings } from "../../store/reducers/settings/generalSettingsSlice";
 import { setNotificationSettings } from "../../store/reducers/settings/notificationSettingsSlice";
 import { setAppearanceSettings } from "../../store/reducers/settings/appearanceSettingsSlice";
 import { setAdvancedSettings } from "../../store/reducers/settings/advancedSettingsSlice";
 
+/**
+ * Thunk action to fetch settings from the server and dispatch them to the store.
+ *
+ * @returns {Function} Thunk action.
+ */
 export const fetchSettingsThunk = () => async (dispatch) => {
   try {
     const response = await fetchSettings();
@@ -16,22 +22,32 @@ export const fetchSettingsThunk = () => async (dispatch) => {
 
     return Promise.resolve(); // Return a resolved promise
   } catch (error) {
-    // Handle the error, maybe dispatch another action to set an error state
-    console.error("Failed to fetch settings:", error);
+    console.error(
+      __("Failed to fetch settings:", "user-request-manager"),
+      error
+    );
     return Promise.reject(error); // Return a rejected promise
   }
 };
 
+/**
+ * Thunk action to save settings to the server.
+ *
+ * @param {Object} settings - The settings object to save.
+ * @returns {Function} Thunk action.
+ */
 export const saveSettingsThunk = (settings) => async (dispatch) => {
   try {
     await saveSettings(settings);
-    console.log("Settings saved successfully!");
+    console.log(__("Settings saved successfully!", "user-request-manager"));
 
     // Re-fetch the settings after saving
     dispatch(fetchSettingsThunk());
   } catch (err) {
-    console.log("Error saving settings: " + err.message);
+    console.log(
+      __("Error saving settings: ", "user-request-manager") + err.message
+    );
   }
 };
 
-// ... other thunks
+// @TODO: Consider handling the error in a more user-friendly way, perhaps by dispatching an action to the store to display an error message to the user.

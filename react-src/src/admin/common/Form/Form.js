6637d622d1c3ea2d7ee35825ee1891c5e0be61a5
Muzammil Hussain <smhz101@gmail.com>
2023-09-18 23:38:36 +0500

    Added JSdocs to the Form component.

--- react-src/src/admin/common/Form/Form.js
+++ react-src/src/admin/common/Form/Form.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./Form.css";
 
+/**
+ * Form component.
+ *
+ * A generic form component to encapsulate and style the form content.
+ *
+ * @param {Object} props - The component's properties.
+ * @param {React.ReactNode} props.children - React children elements.
+ * @param {...Object} props.rest - Any additional props to spread onto the form.
+ * @return {React.ReactNode} Returns the form element with children content.
+ */
 function Form({ children, ...props }) {
   return (
     <form className="urm-form" {...props}>

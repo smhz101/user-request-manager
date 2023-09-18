5e05a68cc280b547ecab1435298c457cd0484b5d
Muzammil Hussain <smhz101@gmail.com>
2023-09-18 23:44:39 +0500

    Added JSdoc comments to the Modal component and fixed the class attribute.

--- react-src/src/admin/common/Modal/Modal.js
+++ react-src/src/admin/common/Modal/Modal.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "./Modal.css";
 
+/**
+ * Modal component.
+ *
+ * A generic modal component to display content in an overlay.
+ *
+ * @param {Object} props - The component's properties.
+ * @param {boolean} props.isOpen - Determines if the modal should be displayed.
+ * @param {function} props.onClose - Function to close the modal.
+ * @param {React.ReactNode} props.children - Content of the modal.
+ * @return {React.ReactNode|null} Returns the modal component if isOpen is true, otherwise null.
+ */
 function Modal({ isOpen, onClose, children }) {
   if (!isOpen) return null;
-
-  console.log("Modal rendered, isOpen:", isOpen);
 
   return (
     <div className="urm-modal-overlay">
       <div className="urm-modal">
         <button className="urm-modal-close" onClick={onClose}>
-          <span class="dashicons dashicons-no-alt"></span>
+          <span className="dashicons dashicons-no-alt"></span>
         </button>
         {children}
       </div>

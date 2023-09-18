ebc2df7c2cbdaa646898abfbee41b166798db8e2
Muzammil Hussain <smhz101@gmail.com>
2023-09-18 23:46:12 +0500

    Added JSdoc and internationalization to the Radio component.

--- react-src/src/admin/common/Radio/Radio.js
+++ react-src/src/admin/common/Radio/Radio.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { __ } from "@wordpress/i18n";
 import "./Radio.css";
 
+/**
+ * Radio component to render a radio input field.
+ *
+ * @param {Object} props Component properties.
+ * @param {string} props.label Label for the radio input.
+ * @returns {JSX.Element} Rendered radio input.
+ */
 function Radio({ label, ...props }) {
   return (
     <label className="urm-radio-group">
       <input type="radio" {...props} />
-      {label}
+      {__(label, "user-request-manager")}
     </label>
   );
 }

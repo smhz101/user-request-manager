0c24809834eb8776a62bc1ef58e10955e8f47b3c
Muzammil Hussain <smhz101@gmail.com>
2023-09-18 23:56:51 +0500

    Added JSdoc and internationalization to the Textarea component.

--- react-src/src/admin/common/Textarea/Textarea.js
+++ react-src/src/admin/common/Textarea/Textarea.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { __ } from "@wordpress/i18n";
 import "./Textarea.css";
 
+/**
+ * Textarea component to render a custom textarea.
+ *
+ * @param {Object} props Component properties.
+ * @param {string} props.label Label for the textarea.
+ * @returns {JSX.Element} Rendered Textarea component.
+ */
 function Textarea({ label, ...props }) {
   return (
     <div className="urm-textarea-group">
-      {label && <label>{label}</label>}
+      {label && <label>{__(label, "user-request-manager")}</label>}
       <textarea className="urm-textarea" {...props}></textarea>
     </div>
   );

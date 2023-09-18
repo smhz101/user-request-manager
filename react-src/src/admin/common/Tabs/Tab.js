caf80e9cc6d49361f4bf178954e7765e1b341737
Muzammil Hussain <smhz101@gmail.com>
2023-09-18 23:54:45 +0500

    Added JSdoc to the Tab component.

--- react-src/src/admin/common/Tabs/Tab.js
+++ react-src/src/admin/common/Tabs/Tab.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+/**
+ * Tab component for displaying tab content.
+ *
+ * @param {Object} props Component properties.
+ * @param {boolean} props.active Indicates if the tab is active.
+ * @param {React.ReactNode} props.children Content to be displayed within the tab.
+ * @returns {JSX.Element} Rendered tab content.
+ */
 function Tab({ children, active }) {
   return (
     <div className={`tab-content ${active ? "active" : ""}`}>{children}</div>

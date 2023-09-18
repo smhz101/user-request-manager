b8c4ad1c1eb5c89f8197a6610e2963e0a40a0b46
Muzammil Hussain <smhz101@gmail.com>
2023-09-18 23:29:06 +0500

    Added JSdocs and internationalization to the Button component.

--- react-src/src/admin/common/Button/Button.js
+++ react-src/src/admin/common/Button/Button.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { __ } from "@wordpress/i18n";
 import "./Button.css";
 
+/**
+ * Button component.
+ *
+ * Renders a custom button.
+ *
+ * @param {Object} props - The component's properties.
+ * @param {React.ReactNode} props.children - The content to be displayed within the button.
+ * @return {React.ReactNode} Returns the button element.
+ */
 function Button({ children, ...props }) {
   return (
     <button className="urm-button" {...props}>
-      {children}
+      {/* @TODO: Ensure that the children passed are string values for proper translation. */}
+      {__(children, "user-request-manager")}
     </button>
   );
 }

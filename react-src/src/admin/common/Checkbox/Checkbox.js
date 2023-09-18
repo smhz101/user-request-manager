eb8bce4f64f81aa88c558d4375a53eef59a03e41
Muzammil Hussain <smhz101@gmail.com>
2023-09-18 23:33:39 +0500

    Added JSdocs and internationalization to the Checkbox component.

--- react-src/src/admin/common/Checkbox/Checkbox.js
+++ react-src/src/admin/common/Checkbox/Checkbox.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { __ } from "@wordpress/i18n";
 import "./Checkbox.css";
 
+/**
+ * Checkbox component.
+ *
+ * Renders a custom checkbox with a label.
+ *
+ * @param {Object} props - The component's properties.
+ * @param {string} props.label - The label to be displayed next to the checkbox.
+ * @return {React.ReactNode} Returns the checkbox element with its label.
+ */
 function Checkbox({ label, ...props }) {
   return (
     <label className="urm-checkbox-group">
       <input type="checkbox" {...props} />
-      {label}
+      {/* @TODO: Ensure that the label passed is a string value for proper translation. */}
+      {__(label, "user-request-manager")}
     </label>
   );
 }

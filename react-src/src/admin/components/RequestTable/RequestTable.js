85bcf5d7ef623286e7b31f3e7c3518f288e8442c
Muzammil Hussain <smhz101@gmail.com>
2023-09-19 00:12:58 +0500

    Added internationalization and JSdoc comments to the RequestTable component.

--- react-src/src/admin/components/RequestTable/RequestTable.js
+++ react-src/src/admin/components/RequestTable/RequestTable.js
@@ -1,22 +1,33 @@
-// RequestTable.js
 import React from "react";
-import { Button, Table } from "../../common"; // Importing the common components
+import { __ } from "@wordpress/i18n";
+import { Button, Table } from "../../common";
 import "./RequestTable.css";
 
+/**
+ * Renders a table to display user requests.
+ *
+ * @param {Object} props - The component's props.
+ * @param {Array} props.requests - An array of user request data.
+ * @param {Function} props.onSelectRequest - A callback function invoked when a request is selected.
+ */
 function RequestTable({ requests, onSelectRequest }) {
   const headers = [
-    { key: "id", label: "ID" },
-    { key: "username", label: "User Name" },
-    { key: "email", label: "Email" },
-    { key: "message", label: "Message" },
-    { key: "status", label: "Status" },
-    { key: "date", label: "Date" },
-    { key: "actions", label: "Actions" },
+    { key: "id", label: __("ID", "user-request-manager") },
+    { key: "username", label: __("User Name", "user-request-manager") },
+    { key: "email", label: __("Email", "user-request-manager") },
+    { key: "message", label: __("Message", "user-request-manager") },
+    { key: "status", label: __("Status", "user-request-manager") },
+    { key: "date", label: __("Date", "user-request-manager") },
+    { key: "actions", label: __("Actions", "user-request-manager") },
   ];
 
   const renderCell = (row, key) => {
     if (key === "actions") {
-      return <Button onClick={() => onSelectRequest(row)}>View Details</Button>;
+      return (
+        <Button onClick={() => onSelectRequest(row)}>
+          {__("View Details", "user-request-manager")}
+        </Button>
+      );
     }
     return row[key];
   };

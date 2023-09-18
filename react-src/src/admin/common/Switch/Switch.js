255ee045da8b3e5b0879622e8a295ff74e70d112
Muzammil Hussain <smhz101@gmail.com>
2023-09-18 23:50:29 +0500

    Added JSdoc and internationalization to the Switch component.

--- react-src/src/admin/common/Switch/Switch.js
+++ react-src/src/admin/common/Switch/Switch.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { __ } from "@wordpress/i18n";
 import "./Switch.css";
 
+/**
+ * Switch component to render a switch toggle input.
+ *
+ * @param {Object} props Component properties.
+ * @param {string} props.label Label for the switch input.
+ * @param {string} [props.className="small"] Optional className to adjust the size of the switch.
+ * @returns {JSX.Element} Rendered switch input.
+ */
 function Switch({ label, className = "small", ...props }) {
   return (
     <label className={`urm-switch-group ${className}`}>
       <input type="checkbox" className="urm-switch-input" {...props} />
       <span className="urm-switch-slider"></span>
-      {label}
+      {label && __(label, "user-request-manager")}
     </label>
   );
 }

d4cbced1c92f18eca4a99eeb9054fc0adb114637
Muzammil Hussain <smhz101@gmail.com>
2023-09-19 00:04:01 +0500

    Cleaned dashboard file.

--- react-src/src/admin/components/Dashboard/Dashboard.js
+++ react-src/src/admin/components/Dashboard/Dashboard.js
@@ -1,32 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { fetchUserRequests } from "../../../services/api";
-import RequestTable from "../RequestTable/RequestTable";
-import RequestDetails from "../RequestDetails/RequestDetails";
-import { Button } from "../../common";
+import React from "react";
 
 function Dashboard() {
-  const [requests, setRequests] = useState([]);
-  const [selectedRequest, setSelectedRequest] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function loadData() {
-      try {
-        const data = await fetchUserRequests();
-        setRequests(data);
-        setLoading(false);
-      } catch (err) {
-        setError(err.message);
-        setLoading(false);
-      }
-    }
-    loadData();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
-
   return (
     <div className="dashboard wrap">
       <header>

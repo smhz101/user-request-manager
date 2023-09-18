77ccee1f389a8f11acbaede1558d942b3d9a7534
Muzammil Hussain <smhz101@gmail.com>
2023-09-18 23:49:26 +0500

    Added JSdoc and internationalization to the Select component.

--- react-src/src/admin/common/Select/Select.js
+++ react-src/src/admin/common/Select/Select.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { __ } from "@wordpress/i18n";
 import "./Select.css";
 
+/**
+ * Select component to render a dropdown select input field.
+ *
+ * @param {Object} props Component properties.
+ * @param {Array<{value: string, label: string}>} props.options Options for the dropdown.
+ * @param {string} props.label Label for the select input.
+ * @returns {JSX.Element} Rendered select input.
+ */
 function Select({ options, label, ...props }) {
   return (
     <div className="urm-select-group">
-      {label && <label>{label}</label>}
+      {label && <label>{__(label, "user-request-manager")}</label>}
       <select className="urm-select" {...props}>
         {options.map((option, index) => (
           <option key={index} value={option.value}>
-            {option.label}
+            {__(option.label, "user-request-manager")}
           </option>
         ))}
       </select>
